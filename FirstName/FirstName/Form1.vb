c017fcc1e1d3efd8bded041ef65fc0da2fd5b894
idsc3101 <idsc3101@gmail.com>
2014-09-15 17:01:19 -0500

    added trim

--- FirstName/FirstName/Form1.vb
+++ FirstName/FirstName/Form1.vb
@@ -4,8 +4,9 @@ Public Class Form1
 
     Private Sub btnFirstName_Click(sender As System.Object, e As System.EventArgs) Handles btnFirstName.Click
         Dim stringerino As String = txtFullName.Text
-        Dim spaceplace As Integer = stringerino.IndexOf(" ")
-        txtFirstName.Text = stringerino.Substring(0, spaceplace)
+        Dim stringerino2 As String = stringerino.Trim()
+        Dim spaceplace As Integer = stringerino2.IndexOf(" ")
+        txtFirstName.Text = stringerino2.Substring(0, spaceplace)
 
     End Sub
 End Class
